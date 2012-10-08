294fcca4aae386b336b95bb4864963075d4bf64e
Stacey Son <sson@FreeBSD.org>
2012-10-08 09:52:34 -0500

    Add rename and renameat syscall shims.

--- work/qemu-1.2.0/bsd-user/syscall.c
+++ work/qemu-1.2.0/bsd-user/syscall.c
@@ -1321,7 +1321,34 @@ do_stat:
 	break;
 
     case TARGET_FREEBSD_NR_rename:
+	{
+		void *p2;
+
+		p = lock_user_string(arg1);
+		p2 = lock_user_string(arg2);
+		if (!p || !p2)
+			ret = -TARGET_EFAULT;
+		else
+			ret = get_errno(rename(p, p2));
+		unlock_user(p2, arg2, 0);
+		unlock_user(p, arg1, 0);
+	}
+	break;
+
     case TARGET_FREEBSD_NR_renameat:
+	{
+		void *p2;
+
+		p  = lock_user_string(arg2);
+		p2 = lock_user_string(arg4);
+		if (!p || !p2)
+			ret = -TARGET_EFAULT;
+		else
+			ret = get_errno(renameat(arg1, p, arg3, p2));
+		unlock_user(p2, arg4, 0);
+		unlock_user(p, arg2, 0);
+	}
+	break;
 
     case TARGET_FREEBSD_NR_link:
     case TARGET_FREEBSD_NR_linkat:
